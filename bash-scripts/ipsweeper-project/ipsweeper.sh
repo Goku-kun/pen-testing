d95b7b9e0cbe4e88b9baa8659e7e160b14651a01
Goku-kun <jethvadharmarajsinh@gmail.com>
2020-10-14 21:45:06 +0530

    command line arg check using if-else

--- bash-scripts/ipsweeper-project/ipsweeper.sh
+++ bash-scripts/ipsweeper-project/ipsweeper.sh
@@ -2,7 +2,13 @@
 #! /bin/bash
 
 # Enter the first three parts of IP address as the command line argument 1
+if [ "$1" == "" ]
+then
+echo "You forgot to enter an IP address"
+echo "Syntax: ./ipsweeper.sh 192.168.0"
 
+else
 for ip in `seq 1 254`; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":"&
 done
+fi
